3b8c5864a0d5298b9677e232f162d3e67dd0fae9
Ayrat Badykov <ayratin555@gmail.com>
2022-02-26 09:44:39 +0200

    add cargo config to library files

--- mix.exs
+++ mix.exs
@@ -32,8 +32,10 @@ defmodule ExPbkdf2.MixProject do
       },
       files: [
         "mix.exs",
+        "native/ex_pbkdf2/.cargo/config",
         "native/ex_pbkdf2/src",
         "native/ex_pbkdf2/Cargo.toml",
+        "native/ex_pbkdf2/Cargo.lock",
         "lib",
         "LICENSE",
         "README.md",
