e4ab7d112de9691fddebf42c545e76085190fd47
Ayrat Badykov <ayratin555@gmail.com>
2023-03-24 10:18:03 +0200

    update pbkdf2 crate and rustler (#26)

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule ExPbkdf2.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.26"},
+      {:rustler, "~> 0.27"},
       {:benchee, "~> 1.0", only: :test},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
