6d98b65744a6d40c718138d2d8df37ee463b2f1f
Ayrat Badykov <ayratin555@gmail.com>
2024-04-07 20:42:54 +0300

    update rustler to 0.32 (#41)

--- lib/ex_pbkdf2/impl.ex
+++ lib/ex_pbkdf2/impl.ex
@@ -9,6 +9,7 @@ defmodule ExPBKDF2.Impl do
     base_url: "https://github.com/ayrat555/ex_pbkdf2/releases/download/v#{version}",
     force_build: System.get_env("RUSTLER_BUILD") in ["1", "true"],
     targets: Enum.uniq(["x86_64-unknown-freebsd" | RustlerPrecompiled.Config.default_targets()]),
+    nif_versions: ["2.15", "2.16"],
     version: version
 
   def generate_salt, do: :erlang.nif_error(:nif_not_loaded)
